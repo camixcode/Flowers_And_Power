2a71d1ecd6998371bc1509d4eb77c0c0098e5825
AlexD30 <yer.vasquez@duocuc.cl>
2022-06-05 18:23:25 -0400

    Home

--- TestDjango/TestDjango/urls.py
+++ TestDjango/TestDjango/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+#from django.conf.urls.static import static
+#from django.conf.urls import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home"),
-    path('',include('core.urls')),
+    path('', include('core.urls')),
 ]
