0223e5a6476930cb92b41bba8c4660d2323bd65a
psicodelicko <101906675+psicodelicko@users.noreply.github.com>
2022-06-05 21:01:25 -0400

    Admin

--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(max_length=50,verbose_name='Nombre de la categoria')
+
+    def __str__(self):
+        return self.nombreCategoria
+
+class Producto(models.Model):
+    idProducto = models.IntegerField(primary_key=True, verbose_name='Id de producto')
+    nombreProducto = models.CharField(max_length=50,verbose_name='Nombre del producto')
+    descripcionProducto = models.CharField(max_length=50,verbose_name='Descripcion del producto')
+    precioProducto = models.IntegerField(verbose_name='precion del producto')
+
+    def __str__(self):
+        return self.nombreProducto
+
