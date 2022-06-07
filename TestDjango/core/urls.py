5548c669f917f57da666056760c8e8847f5cc20b
Cami <101901288+camixcode@users.noreply.github.com>
2022-06-06 22:51:41 -0400

    2.0

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
-from .views import home
+
+from TestDjango.core.models import Producto
+from .views import Arbusto, Categoria, Contacto, F_Crear_Cuenta, HistoricoCompra, InicioSesion1, Macetero, Nosotros, Paypal, PerfilProducto, Seguimiento, Tierra, home, index_home
 
 urlpatterns = [
     path('', home, name="home"),
+    path('', Arbusto, name="Arbusto"),
+    path('', Categoria, name="Categoria"),
+    path('', Contacto, name="Contacto"),
+    path('', F_Crear_Cuenta, name="F_crear_Cuenta"),
+    path('', HistoricoCompra, name="HistoricoCompra"),
+    path('', InicioSesion1, name="InicioSesion1"),
+    path('', Macetero, name="Macetero"),
+    path('', Nosotros, name="Nosotros"),
+    path('', Paypal, name="Paypal"),
+    path('', PerfilProducto, name="PerfilProducto"),
+    path('', Producto, name="Producto"),
+    path('', Seguimiento, name="Seguimiento"),
+    path('', Tierra, name="Tierra"),
+    path('', index_home, name="index_home"),
 
 ]
