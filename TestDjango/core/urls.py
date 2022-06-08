c25cb4f3ee0513cd919d8bb5051467480bffadec
AlexD30 <yer.vasquez@duocuc.cl>
2022-06-08 01:32:59 -0400

    Vista Nosotros

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -4,18 +4,27 @@ from django.urls import path
 
 
 
-from .views import Arbusto, home, Contacto
+from .views import Arbusto, home, Contacto, Categoria1, F_Crear_Cuenta,form_mod_usuario,Nosotros,HistoricoCompra,index_home,InicioSesion1,listado_producto ,Paypal,PerfilProducto,Producto,Seguimiento,Tierra, Macetero
 
-
-
-from .views import  home,Arbusto
 
 urlpatterns = [
     path('', home, name="home"),
     path('Arbusto/', Arbusto, name="Arbusto"),
     path('Contacto/', Contacto, name="Contacto"),
-
-
+    path('Categoria1/', Categoria1, name="Categoria1"),
+    path('Crear Cuenta/', F_Crear_Cuenta, name="Crear Cuenta"),
+    path('Formulario Usuario/', form_mod_usuario, name="Formulario Usuario"),
+    path('Historico de Compras/', HistoricoCompra, name="Historico de Compras"),
+    path('Home/', index_home, name="Home"),
+    path('Inicio de Sesion/', InicioSesion1, name="Inicio de Sesion"),
+    path('Lista de productos/', listado_producto, name="Lista de productos"),
+    path('Producto de Macetero/', Macetero, name="Producto de Macetero"),
+    path('Nosotros/', Nosotros, name="Nosotros"),
+    path('Paypal/', Paypal, name="Paypal"),
+    path('Perfil de Producto/', PerfilProducto, name="Perfil de Producto"),
+    path('Producto/', Producto, name="Producto"),
+    path('Seguimiento de Compra/', Seguimiento, name="Seguimiento de Compra"),
+    path('Producto de Tierra/', Tierra, name="Tierra"),
 
 ]
 
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -30,3 +30,45 @@ def Arbusto(request):
 
 def Contacto(request):
     return render(request, 'core/Contacto.html')
+
+def Categoria1(request):
+    return render(request, 'core/Categoria1.html')
+
+def F_Crear_Cuenta(request):
+    return render(request, 'core/F_Crear_Cuenta.html')
+
+def form_mod_usuario(request):
+    return render(request, 'core/form_mod_usuario.html')
+
+def HistoricoCompra(request):
+    return render(request, 'core/HistoricoCompra.html')
+
+def index_home(request):
+    return render(request, 'core/index_home.html')    
+
+def InicioSesion1(request):
+    return render(request, 'core/InicioSesion1.html')        
+
+def listado_producto(request):
+    return render(request, 'core/listado_producto.html')    
+
+def Macetero(request):
+    return render(request, 'core/Macetero.html')    
+
+def Nosotros(request):
+    return render(request, 'core/Nosotros.html')                
+
+def Paypal(request):
+    return render(request, 'core/Paypal.html')   
+
+def PerfilProducto(request):
+    return render(request, 'core/PerfilProducto.html')      
+
+def Producto(request):
+    return render(request, 'core/Producto.html')      
+
+def Seguimiento(request):
+    return render(request, 'core/Seguimiento.html')      
+
+def Tierra(request):
+    return render(request, 'core/Tierra.html')                          
