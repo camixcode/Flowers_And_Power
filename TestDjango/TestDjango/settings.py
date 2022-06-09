7fadddcf2b21579491e822644b945f09ef4b494d
psicodelicko <101906675+psicodelicko@users.noreply.github.com>
2022-06-08 23:42:28 -0400

    Static url

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/ holis
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
