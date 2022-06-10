f3a964b47df67c0ae17952584acf8c0bb6a3bfc6
AlexD30 <yer.vasquez@duocuc.cl>
2022-06-09 22:54:04 -0400

    BD

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #from TestDjango.core.models import Producto
 
 
@@ -31,6 +33,9 @@ urlpatterns = [
     path('Seguimiento/', Seguimiento, name="Seguimiento"),
     path('Tierra/', Tierra, name="Tierra"),
     #path('Nav/', NavBar, name="Nav"),
-
 ]
 
+
+urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+##urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
