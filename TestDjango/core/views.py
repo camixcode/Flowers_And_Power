c33b51f51ff46934ecac50fd6ab6def5b1b80709
AlexD30 <yer.vasquez@duocuc.cl>
2022-07-01 00:44:08 -0400

    ForJson

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -23,32 +23,6 @@ from .serializers import ProductoSerializer
 from .models import Producto, Usuario
 
 
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-from core.models import Producto
-from .serializers import ProductoSerializer
-
-
-@csrf_exempt
-@api_view (['GET', 'POST'])
-def lista_producto(request):
-    if request.method == 'GET':
-        producto = Producto.objects.all()
-        serializers = ProductoSerializer(producto, many=True)
-        return Response(serializers.data)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ProductoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_2O1_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_BAD_REQUEST) 
-
 
 
 
